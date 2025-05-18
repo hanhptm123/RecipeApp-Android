510c9116220092b42cd1431578f2f587252dfbc5
hanhptm123 <studyhanh@gmail.com>
2025-05-18 11:15:59 +0700

    feature: add feature logout

--- app/src/main/java/com/example/recipeapp/Admin_page.java
+++ app/src/main/java/com/example/recipeapp/Admin_page.java
@@ -2,6 +2,7 @@ package com.example.recipeapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -23,6 +24,28 @@ public class Admin_page extends AppCompatActivity {
                 finish();
             }
         });
+        ImageButton btnlogout = (ImageButton) findViewById(R.id.btnLogout);
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new androidx.appcompat.app.AlertDialog.Builder(Admin_page.this)
+                        .setTitle("Confirm Logout")
+                        .setMessage("Are you sure you want to logout?")
+                        .setPositiveButton("Yes", (dialog, which) -> {
+                            // Xóa session SharedPreferences
+                            getSharedPreferences("UserPrefs", MODE_PRIVATE).edit().clear().apply();
+
+                            // Chuyển về màn hình đăng nhập
+                            Intent intent = new Intent(Admin_page.this, Login.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish();
+                        })
+                        .setNegativeButton("No", null) // Không làm gì nếu chọn "No"
+                        .show();
+            }
+        });
+
 
     }
 }
