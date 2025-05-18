421d56b10a6c4b15a90b6fd196864f8c4f6b689b
hanhptm123 <studyhanh@gmail.com>
2025-05-18 16:38:15 +0700

    feature: add feature view profile and edit profile

---
+++ app/src/main/java/com/example/recipeapp/User_home_page.java
@@ -0,0 +1,56 @@
+package com.example.recipeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class User_home_page extends AppCompatActivity {
+    KET_NOI_CSDL dbHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_user_home_page);
+        ImageButton btnlogout = (ImageButton)findViewById(R.id.userbtnLogout);
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new androidx.appcompat.app.AlertDialog.Builder(User_home_page.this)
+                        .setTitle("Confirm Logout")
+                        .setMessage("Are you sure you want to logout?")
+                        .setPositiveButton("Yes", (dialog, which) -> {
+                            // Xóa session SharedPreferences
+                            getSharedPreferences("UserPrefs", MODE_PRIVATE).edit().clear().apply();
+
+                            // Chuyển về màn hình đăng nhập
+                            Intent intent = new Intent(User_home_page.this, Login.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish();
+                        })
+                        .setNegativeButton("No", null) // Không làm gì nếu chọn "No"
+                        .show();
+            }
+        });
+        ImageButton btnhome = (ImageButton) findViewById(R.id.userBtnHome);
+        btnhome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(User_home_page.this, User_page.class);
+                startActivity(it);
+                finish();
+            }
+        });
+        ImageButton btn_my_profile = (ImageButton) findViewById(R.id.userProfileIcon);
+        btn_my_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(User_home_page.this, User_profile.class);
+                startActivity(it);
+            }
+        });
+
+    }
+}
